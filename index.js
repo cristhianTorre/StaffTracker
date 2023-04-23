c665645a52ac3ccd752c2f90c53bc229678aad6b
cristhianTorre <cristhian.torres@mail.escuelaing.edu.co>
2023-04-22 23:09:23 -0500

    Front - Arreglado, duplicado de proyectos debido a espacios en excel

--- index.js
+++ index.js
@@ -28,19 +28,18 @@ app.get('/', function(req, res){
 
     function pintarProyecto(){
         //console.log('Funciona');
-        var texthtml = '<div class="containerProject row">';               
+        var texthtml = '';               
         const valores = getProyectos('CORE','Cross/Gestor documental y SSDD');
         for(var i=0; i<valores.length; i++){
             //var proyecto = valores[i][1];
             var empleados = valores[i][0];
-            texthtml += '<div class="cardProyecto col-sm-4"><h2 id="titleProject" class="titles">Proyecto: '+valores[i][1][0]+'</h2><ol id="listProject" class="list"><li>Descripción: '+valores[i][1][1]+'</li><li>Tipo: '+valores[i][1][2]+'</li><li>Scrum: '+valores[i][1][3]+'</li><li>Product Owner: '+valores[i][1][4]+'</li><li>Estado: '+valores[i][1][5]+'</li></ol></div>';
+            texthtml += '<div class="containerProject row"><div class="cardProyecto col-sm-4"><h2 id="titleProject" class="titles">Proyecto: '+valores[i][1][0]+'</h2><ol id="listProject" class="list"><li>Descripción: '+valores[i][1][1]+'</li><li>Tipo: '+valores[i][1][2]+'</li><li>Scrum: '+valores[i][1][3]+'</li><li>Product Owner: '+valores[i][1][4]+'</li><li>Estado: '+valores[i][1][5]+'</li></ol></div><div class="othersProyecto col-sm-8"><div class="persons row"><h2 class="titles">Personas: </h2><ol class="listPerson">';
             for(var j=0; j<empleados.length; j++){
                 //var empleado = valores[i][0][j];
-                texthtml += '<div class="othersProyecto col-sm-8"><div class="persons row"><h2 class="titles">Personas: </h2><ol class="listPerson"><li class="itemListPerson"><div class="person"><h5  class="person personCode">'+valores[i][0][j][0]+'</h5><h6  class="person">'+valores[i][0][j][1]+'</h6><img class="imagePerson" src="https://img.freepik.com/vector-premium/icono-usuario-hombre-traje-negocios_454641-453.jpg?w=50" class="img-fluid rounded-start" alt="..."><p class=""><small class="text-muted">'+valores[i][0][j][2]+' / '+valores[i][0][j][3]+' / '+valores[i][0][j][4]+'</small></p></div></li></ol></div>';
+                texthtml += '<li class="itemListPerson"><div class="person"><h5  class="person personCode">'+valores[i][0][j][0]+'</h5><h6  class="person">'+valores[i][0][j][1]+'</h6><img class="imagePerson" src="https://img.freepik.com/vector-premium/icono-usuario-hombre-traje-negocios_454641-453.jpg?w=50" class="img-fluid rounded-start" alt="..."><p class=""><small class="text-muted">'+valores[i][0][j][2]+' / '+valores[i][0][j][3]+' / '+valores[i][0][j][4]+'</small></p></div></li>';
             }
-            texthtml += '<div class="containerSchedule row"><h2 class="titles">cronograma: </h2><ol class="listTrimestre"><li class="itemListTrimestre"><div class="person"><h5  class="person personCode">Primer Trimestre</h5><ol><li>'+valores[i][1][6]+'</li><li>'+valores[i][1][7]+'</li></ol></div></li></ol></div></div>';
+            texthtml += '</ol></div></div><div class="containerSchedule row"><h2 class="titles">cronograma: </h2><ol class="listTrimestre"><li class="itemListTrimestre"><div class="person"><h5  class="person personCode">Primer Trimestre</h5><ol><li>'+valores[i][1][6]+'</li><li>'+valores[i][1][7]+'</li></ol></div></li></ol></div></div>';
         }
-        texthtml += '</div>';
         return texthtml;
         //console.log('Funciona2');
         //document.getElementById('contenedorValores').innerHTML = texthtml;
@@ -114,6 +113,7 @@ app.get('/', function(req, res){
         for(const itemFila of leerStaffing){
             if(itemFila['Servicio'] == servicio && itemFila['Dirección'] == direccion && !(itemFila['Proyecto'] in proyectos)){
                 informacion.push([getEmpleados(itemFila['Proyecto']), consultaInformacionProyecto(itemFila['Proyecto'])]);
+                //console.log(itemFila['Proyecto']);
                 proyectos.push(itemFila['Proyecto']);
             }
         }
