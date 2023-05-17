cb8627495fbdaff4248819d68948454548d43f5a
Daniel Alejandro Larrota Moreno <daniellarrota96@gmail.com>
2023-05-17 15:14:09 -0500

    Cambios minimos visuales

--- routes/connect.js
+++ routes/connect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const conexion = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'S3rv3r',
   database: 'staffing_project',
   multipleStatements: true
 });
